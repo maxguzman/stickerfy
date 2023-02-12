1544bfddeb1b441157a9c9905e0fa8e3c6675bbd
Max Guzman <max.guzman@icloud.com>
2023-02-12 19:24:44 +0100

    fix: product repository with implementation

--- app/repositories/order_repository.go
+++ app/repositories/order_repository.go
@@ -1,9 +1,34 @@
 package repositories
 
-import "stickerfy/app/models"
+import (
+	"stickerfy/app/models"
+	"stickerfy/pkg/platform/database"
+)
 
 // OrderRepository is an interface for an order repository
 type OrderRepository interface {
 	GetAll() ([]models.Order, error)
 	Post(order models.Order) error
+}
+
+// orderRepository is a implementation of OrderRepository
+type orderRepository struct{
+	client database.Client
+}
+
+// NewOrderRepository creates a new OrderRepository
+func NewOrderRepository(c database.Client) OrderRepository {
+	return &orderRepository{
+		client: c,
+	}
+}
+
+// GetAll returns all orders
+func (or *orderRepository) GetAll() ([]models.Order, error) {
+	return []models.Order{}, nil
+}
+
+// Post creates a new order
+func (or *orderRepository) Post(order models.Order) error {
+	return nil
 }
--- app/repositories/product_repository.go
+++ app/repositories/product_repository.go
@@ -3,6 +3,8 @@ package repositories
 import (
 	"stickerfy/app/models"
 
+	"stickerfy/pkg/platform/database"
+
 	"github.com/google/uuid"
 )
 
@@ -13,4 +15,41 @@ type ProductRepository interface {
 	Post(product models.Product) error
 	Update(product models.Product) error
 	Delete(product models.Product) error
+}
+
+// productRepository is a implementation of ProductRepository
+type productRepository struct{
+	client database.Client
+}
+
+// NewProductRepository creates a new ProductRepository
+func NewProductRepository(c database.Client) ProductRepository {
+	return &productRepository{
+		client: c,
+	}
+}
+
+// FindAll returns all products
+func (pr *productRepository) GetAll() ([]models.Product, error) {
+	return []models.Product{}, nil
+}
+
+// Get returns a product by id
+func (pr *productRepository) GetByID(id uuid.UUID) (models.Product, error) {
+	return models.Product{}, nil
+}
+
+// New creates a new product
+func (pr *productRepository) Post(product models.Product) error {
+	return nil
+}
+
+// Update updates a product
+func (pr *productRepository) Update(product models.Product) error {
+	return nil
+}
+
+// Delete deletes a product by id
+func (pr *productRepository) Delete(product models.Product) error {
+	return nil
 }
